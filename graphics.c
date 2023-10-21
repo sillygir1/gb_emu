9d0e01273cdd4e4fa8284d225f0236739c2df666
sillygir1 <25554397+sillygir1@users.noreply.github.com>
2023-10-22 01:17:20 +0400

    Add scrolling to render (SCY and SCX registers).

--- graphics.c
+++ graphics.c
@@ -1,6 +1,16 @@
 #include "graphics.h"
 #include "system.h"
 
+uint8_t color_palette[4][3] = {{0x00, 0x00, 0x00},
+			       {0x67, 0x67, 0x67},
+			       {0xb6, 0xb6, 0xb6},
+			       {0xff, 0xff, 0xff}};
+
+uint8_t color_palette_inverted[4][3] = {{0xff, 0xff, 0xff},
+					{0xb6, 0xb6, 0xb6},
+					{0x67, 0x67, 0x67},
+					{0x00, 0x00, 0x00}};
+
 bool window_init(System *system) {
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		// throw some error idk
@@ -8,56 +18,47 @@ bool window_init(System *system) {
 	}
 	system->graphics->window = SDL_CreateWindow(
 	    "gb_emu", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-	    width * system->settings->pixel_scale,
-	    height * system->settings->pixel_scale,
+	    width_viewport * _pixel_scale, height_viewport * _pixel_scale,
 	    SDL_WINDOW_SHOWN | SDL_WINDOW_UTILITY);
 
 	system->graphics->renderer = SDL_CreateRenderer(
 	    system->graphics->window, -1, SDL_RENDERER_ACCELERATED);
+
+	for (uint8_t i = 0; i < height_virtual / 8; i++) {
+		for (uint8_t j = 0; j < width_virtual / 8; j++) {
+			system->graphics->tile_rect[i][j].x =
+			    8 * j * _pixel_scale;
+			system->graphics->tile_rect[i][j].y =
+			    8 * i * _pixel_scale;
+			system->graphics->tile_rect[i][j].w = _8px;
+			system->graphics->tile_rect[i][j].h = _8px;
+		}
+	}
 	return true;
 }
 
 void draw_pixel(System *system, uint8_t x, uint8_t y) {
 	SDL_Rect r;
-	r.x = x * system->settings->pixel_scale;
-	r.y = y * system->settings->pixel_scale;
-	r.w = system->settings->pixel_scale;
-	r.h = system->settings->pixel_scale;
+	r.x = x * _pixel_scale;
+	r.y = y * _pixel_scale;
+	r.w = _pixel_scale;
+	r.h = _pixel_scale;
 
 	SDL_SetRenderDrawColor(system->graphics->renderer, 0, 0, 0, 255);
 
 	SDL_RenderFillRect(system->graphics->renderer, &r);
 }
 
-void draw_tiles(System *system, uint16_t start) {
-	if (start) { // for testing purposes
-		     //
-	} else {
-		//
-	}
-}
-
 int render(System *system) {
-	uint8_t w = 256 / 8;
-	uint8_t h = 256 / 8;
+	uint8_t w = width_virtual / 8;
+	uint8_t h = height_virtual / 8;
 
 	SDL_Texture *texture = SDL_CreateTexture(
 	    system->graphics->renderer, SDL_PIXELFORMAT_RGBA8888,
 	    SDL_TEXTUREACCESS_STREAMING, 8, 8);
 
-	for (uint8_t i = 0; i < h; i++) {
-		for (uint8_t j = 0; j < w; j++) {
-			system->graphics->tile_rect[i][j].x =
-			    8 * j * system->settings->pixel_scale;
-			system->graphics->tile_rect[i][j].y =
-			    8 * i * system->settings->pixel_scale;
-			system->graphics->tile_rect[i][j].w = _8px;
-			system->graphics->tile_rect[i][j].h = _8px;
-		}
-	}
-
-	unsigned int a = SDL_GetTicks();
-	unsigned int b = SDL_GetTicks();
+	uint64_t a = SDL_GetTicks64();
+	uint64_t b = SDL_GetTicks64();
 	double delta = 0;
 
 	// Window display loop
@@ -68,14 +69,39 @@ int render(System *system) {
 			switch (e.type) {
 			case SDL_QUIT:
 				keep_window_open = false;
+				return 1;
 				break;
 			}
 		}
-		a = SDL_GetTicks();
+
+		uint8_t SCX = system->memory[0xFF43];
+		uint8_t SCY = system->memory[0xFF42];
+		uint16_t x_pos = SCX * _pixel_scale;
+		uint16_t y_pos = SCY * _pixel_scale;
+
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				int x = 8 * j * _pixel_scale + x_pos;
+				if (x > 255 * _pixel_scale)
+					x -= (int)(256 * _pixel_scale);
+				int y = 8 * i * _pixel_scale + y_pos;
+				if (y > 255 * _pixel_scale)
+					y -= 256 * _pixel_scale;
+				system->graphics->tile_rect[i][j].x = x;
+				system->graphics->tile_rect[i][j].y = y;
+				system->graphics->tile_rect[i][j].w = _8px;
+				system->graphics->tile_rect[i][j].h = _8px;
+			}
+		}
+
+		a = SDL_GetTicks64();
 		delta = a - b;
 		if (delta > 1000 / 60.0) {
-			SDL_SetRenderDrawColor(system->graphics->renderer, 0, 0,
-					       0, 255);
+			system->memory[0xFF43] -= 1;
+			// system->memory[0xFF42] += 1;
+			SDL_SetRenderDrawColor(system->graphics->renderer, 255,
+					       255, 255, 255);
+			// if ( /*get LCDC lcd off flag */ ) continue;
 			SDL_RenderClear(system->graphics->renderer);
 			for (uint8_t i = 0; i < h; i++) {
 				for (uint8_t j = 0; j < w; j++) {
@@ -83,7 +109,8 @@ int render(System *system) {
 					    0x8000 +
 					    system->memory[0x9800 + i * w + j] *
 						16;
-					get_tile(system->memory, texture, addr);
+					get_tile(system->memory, texture, addr,
+						 color_palette_inverted);
 					SDL_RenderCopy(
 					    system->graphics->renderer, texture,
 					    NULL,
@@ -92,6 +119,8 @@ int render(System *system) {
 			}
 
 			SDL_RenderPresent(system->graphics->renderer);
+
+			b = SDL_GetTicks64();
 		}
 	}
 	return 0;
--- graphics.h
+++ graphics.h
@@ -2,10 +2,13 @@
 #include "gb-bytes-to-image/tile.h"
 #include "system.h"
 
-#define width 160  // px
-#define height 144 // px
+#define width_viewport 160  // px, actual screen dimension
+#define height_viewport 144 // px, actual screen dimension
+#define height_virtual 256  // px
+#define width_virtual 256   // px
 
 #define _8px system->settings->pixel_scale * 8 // tile size
+#define _pixel_scale system->settings->pixel_scale
 
 bool window_init(System *system);
 
